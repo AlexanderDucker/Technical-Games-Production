97cc804db87d6edb833b9d3df8070ae0a6ac66ca
Applesourze <l014269c@student.staffs.ac.uk>
2015-02-03 11:06:44 +0000

    Movement
    Basic Framework with movement code added and gravity removed TL

--- SneakyPiledrivers/MonochromeRainbow/Classes/Player.cs
+++ SneakyPiledrivers/MonochromeRainbow/Classes/Player.cs
@@ -233,6 +233,18 @@ namespace MonochromeRainbow
 					//Aim Right
 					facingDirection = 4;
         		}
+				//Up Vertical movement.
+        		if ((gamePadData.Buttons & GamePadButtons.Up) != 0)
+        		{
+					yVelocity = 4.0f;
+					
+        		}
+				//Down Vertical movement.
+        		if ((gamePadData.Buttons & GamePadButtons.Down) != 0)
+        		{
+					yVelocity = -4.0f;
+        		}
+				
 			}
 			if ((gamePadData.Buttons & GamePadButtons.R) != 0)
 			{
@@ -274,17 +286,11 @@ namespace MonochromeRainbow
         		}
 			}
 					
-			//Slow down player if button is held.
-			if ((gamePadData.Buttons & GamePadButtons.Cross) != 0 & !onGround && yVelocity > 0)
-			{
-        		yVelocity += 0.1f;
-			}
+			
 			
 			//Check if player is off the ground.
 			if (!onGround)
 			{
-				//Player loses vertical speed tue to gravity.
-				yVelocity -= 0.5f;
 				
 				if ((gamePadData.Buttons & GamePadButtons.Left) == 0 && (gamePadData.Buttons & GamePadButtons.Right) == 0)
 				{
@@ -303,24 +309,24 @@ namespace MonochromeRainbow
 				}
 			}
 
-			//Player shouldn't fall too fast. [Terminal Velocity]
-			if (yVelocity < -5.0f)
-			{
-        		yVelocity = -5.0f;
-			}
+			////Player shouldn't fall too fast. [Terminal Velocity]
+			//if (yVelocity < -5.0f)
+			//{
+        	//	yVelocity = -5.0f;
+			//}
 			
 			//Check if player is on the ground.
-            if (yVelocity != 0.0f)
-			{
-				onGround = false;
-    		}
+            //if (yVelocity != 0.0f)
+			//{
+			//	onGround = false;
+    		//}
 			
 			//Check if player has hit the ground.
-			if (playerPos.Y < 0.0f)
-			{
-				playerPos.Y = 0.0f;
-				onGround = true;
-			}
+			//if (playerPos.Y < 0.0f)
+			//{
+			//	playerPos.Y = 0.0f;
+			//	onGround = true;
+			//}
 						
 			//Update player position.
     		playerPos.Y += yVelocity;
